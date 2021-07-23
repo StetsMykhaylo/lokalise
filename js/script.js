deeffa3f83c150aa95f8aa29e8f29649ad953f43
StetsMykhaylo <75065477+StetsMykhaylo@users.noreply.github.com>
2021-07-23 17:32:43 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+$(document).ready(function () {
+
+
+
+
+    $('.one').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    });
+
+
+
+
+
+
+    // $('.one').slick({
+    //     centerMode: true,
+    //     centerPadding: '60px',
+    //     dots: true,
+    //     slidesToShow: 1,
+    //     dotsClass: 'slick-dot',
+    //     responsive: [
+    //         {
+    //             breakpoint: 768,
+    //             settings: {
+    //                 arrows: false,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 3
+    //             }
+    //         },
+    //         {
+    //             breakpoint: 480,
+    //             settings: {
+    //                 arrows: false,
+    //                 centerMode: true,
+    //                 centerPadding: '40px',
+    //                 slidesToShow: 1
+    //             }
+    //         }
+    //     ]
+    // });
+
+
+
+
+});
